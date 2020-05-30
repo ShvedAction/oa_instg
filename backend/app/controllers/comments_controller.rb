bce7810222ab34169f6fe4511388cb02ef91ecc7
shved <shvedujk@gmail.com>
2020-05-30 15:22:54 +0300

    includes user for comments

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.all.includes(:user)
   end
 
   # GET /comments/1
