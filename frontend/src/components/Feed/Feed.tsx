c3fd1d1389adcf1636088963ace18ab71280c3a4
shved <shvedujk@gmail.com>
2020-05-31 20:57:21 +0300

    unused

--- frontend/src/components/Feed/Feed.tsx
+++ frontend/src/components/Feed/Feed.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import { IFeedProps, IFeedItem } from "./interfaces";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from "./Card";
